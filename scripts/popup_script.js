5ea7536ce3bfd2dfb8ebd08805fd00e38fe9eece
Hassan Rehan <hassanrehan@tazteqpk.com>
2021-06-22 06:10:36 -0700

    solving CORS errors

--- scripts/popup_script.js
+++ scripts/popup_script.js
@@ -12,7 +12,6 @@ function validURL(str) {
 //function for calling ProxyCrawl api
 function call_pc_api(event){
     event.preventDefault();
-    try{
         let a_url = document.getElementById("a_url").value;
         let token = document.getElementById("token").value;
         if(validURL(a_url) && token !=""){
@@ -20,58 +19,23 @@ function call_pc_api(event){
             //restricting multiple api calls
             document.getElementById("submit_btn").innerHTML="<i class='fa fa-spinner fa-pulse fa-fw' aria-hidden='true'></i>"
             document.getElementById("submit_btn").disabled = true;
-
-            proxy_url = "api.proxycrawl.com/scraper?token="+token+"&url="+a_url;
-            // $.ajax({
-            //     type: "GET",
-            //     url: proxy_url,
-            //     dataType : "json",
-            //     cache: false,
-            //     timeout: 10000,
-            //     success: function (data){
-            //         document.getElementById("result").innerHTML= data;
-                    
-            //         //resetting things
-            //         document.getElementById("submit_btn").innerHTML="Submit"
-            //         document.getElementById("submit_btn").disabled = false;
-            //     },
-            //     error: function (e) {
-            //         if(e.statusText == 'timeout')
-            //             document.getElementById("result").innerHTML="Timeout. Please try again.";
-            //         else
-            //         document.getElementById("result").innerHTML="Please enter Valid amazon product url and ProxyCrawl Token.";
-                    
-            //         //resetting things
-            //         document.getElementById("submit_btn").innerHTML="Submit"
-            //         document.getElementById("submit_btn").disabled = false;
-            //     }
-            // });
+            proxy_url = "https://api.proxycrawl.com/scraper?token="+token+"&url="+a_url;
             
-            
-            options = {
-                method  : 'GET', 
-                headers : new Headers({
-                    "User-Agent"   : "Mozilla/5.0",
-                    "Access-Control-Allow-Origin": "*"
-                })
-            }
-
-            fetch(proxy_url, options).then(function (response) {
+            fetch(proxy_url).then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
-            });
+                document.getElementById("result").innerHTML=JSON.stringify(data);
+            }).catch(function(){
+                document.getElementById("result").innerHTML="Something went worng. Please double check amazon product url and ProxyCrawl Token.";
+            }).finally(function() {
+                //restricting multipl api calls
+                document.getElementById("submit_btn").innerHTML="Submit"
+                document.getElementById("submit_btn").disabled = false;
+             });
 
         }else
             document.getElementById("result").innerHTML="Url or token is not valid.";
-
-    }catch(e){
-        document.getElementById("result").innerHTML="Something went worng. Please double check amazon product url and ProxyCrawl Token.";
-        //resetting things
-        document.getElementById("submit_btn").innerHTML="Submit"
-        document.getElementById("submit_btn").disabled = false;
-    }
 }
 
 document.getElementById("amazon-form").addEventListener("submit",call_pc_api);
